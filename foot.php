4e9ff731960021608c39d1d1e622b936ac747371
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-10-05 15:56:53 +0100

    second commit

--- foot.php
+++ foot.php
@@ -1,44 +1,44 @@
 
 </main>
+
 <aside>
 
+	<ul>
+		<li>Year 1
 			<ul>
-				<li>Year 1
-					<ul>
-						<li><a href="csy1014.php">CSY1014 Computer Systems</a></li>
-						<li><a href="csy1017.php">CSY1017 Computer Communications</a></li>
-						<li><a href="csy1018.php">CSY1018 Internet Technology</a></li>
-						<li><a href="csy1019.php">CSY1019 Software Engineering 1</a></li>
-					</ul>
-				</li>
-				<li>Year 2
-					<ul>
-						<li><a href="csy2001.php">CSY2001 Computer Networks</a></li>
-						<li><a href="csy2002.php">CSY2002 Operating Systems</a></li>
-						<li><a href="csy2006.php">CSY2006 Software Engineering 2</a></li>
-						<li><a href="csy2027.php">CSY2027 Group Project</a></li>
-						<li><a href="csy2028.php">CSY2028 Web programming</a></li>
-					</ul>
-				</li>
-
-				</li>
-				<li>Year 3
-
-					<ul>
-						<li><a href="csy3010.php">CSY3010 Media Technology</a></li>
-						<li><a href="csy3013.php">CSY3013 Software Engineering 3</a></li>
-						<li><a href="csy3015.php">CSY3015 Real Time and and Embedded Systems</a></li>
-						<li><a href="csy4010.php">CSY4010 Computing dissertation</a></li>
-					</ul>
-				</li>
+				<li><a href="csy1014.html">CSY1014 Computer Systems</a></li>
+				<li><a href="csy1017.html">CSY1017 Computer Communications</a></li>
+				<li><a href="csy1018.html">CSY1018 Internet Technology</a></li>
+				<li><a href="csy1019.html">CSY1019 Software Engineering 1</a></li>
 			</ul>
+		</li>
+		<li>Year 2
+			<ul>
+				<li><a href="csy2001.html">CSY2001 Computer Networks</a></li>
+				<li><a href="csy2002.html">CSY2002 Operating Systems</a></li>
+				<li><a href="csy2006.html">CSY2006 Software Engineering 2</a></li>
+				<li><a href="csy2027.html">CSY2027 Group Project</a></li>
+			</ul>
+		</li>
 
-		</aside>
+		</li>
+		<li>Year 3
 
-		<footer>
-			&copy; Small text at the bottom just because it seems necessary
-		</footer>
+			<ul>
+				<li><a href="csy3010.html">CSY3010 Media Technology</a></li>
+				<li><a href="csy3013.html">CSY3013 Software Engineering 3</a></li>
+				<li><a href="csy3015.html">CSY3015 Real Time and and Embedded Systems</a></li>
+				<li><a href="csy4010.html">CSY4010 Computing dissertation</a></li>
+			</ul>
+		</li>
+	</ul>
 
-	</body>
+</aside>
+
+<footer>
+	&copy; Small text at the bottom just because it seems necessary
+</footer>
+
+</body>
 
 </html>
