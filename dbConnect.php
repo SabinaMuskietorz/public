56462d22eede7a4e8110d4aa3dab03ffff0ce0b3
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-12-01 11:07:21 +0000

    first commit

--- dbConnect.php
+++ dbConnect.php
@@ -8,9 +8,9 @@ $schema = 'CSY2028';
 $pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,
            [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$results = $pdo=>query('SELECT * FROM person');
+$results = $pdo ->query('SELECT * FROM person');
 
 foreach ($results as $row) {
-    echo '<p>' . $row['firstname'] . ' ' .$row['surname'] . 'was born ' . $row['birthday'] . '</p>';
+    echo '<p>' . $row['firstname'] . ' ' .$row['surname'] . ' was born ' . $row['birthday'] . '</p>';
 }
 ?>
